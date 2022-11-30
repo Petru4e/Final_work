7596ee4b29c62d90fec20f6c7bc58f06834187ee
Petru4e <Petru4e85@mail.ru>
2022-11-30 21:03:06 +0300

    Task solution

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter any words separated by a space");
+string[] words = Console.ReadLine()!.Split(" ");
+
+PrintArray(ArraySort(words));
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) Console.Write($"{array[i]} ");
+        else Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+
+string[] ArraySort(string[] array)
+{
+    int lengthResult = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3 && array[i].Length!=0) lengthResult++;
+    }
+
+    string[] result = new string[lengthResult];
+    lengthResult = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3 && array[i].Length!=0)
+        {
+            result[lengthResult] = array[i];
+            lengthResult++;
+        }
+    }
+    return result;
+}
